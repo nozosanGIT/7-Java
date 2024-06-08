451177ca7534041e1d24aef852051e28195a7125
nozosanGIT <nozosan3443@gmail.com>
2024-06-08 19:01:19 +0900

    Add files via upload
    基礎4－1

---
+++ prob4_1.java
@@ -0,0 +1,16 @@
+package issyuukan_kiso4;
+
+public class prob4_1 {
+public static void main(String...args){
+	
+	
+	int num=(int)(Math.random()*10)+1;
+	
+	System.out.println(num);
+	for(int i=0; i<num; i++) {
+		
+		System.out.print("■");
+	}		
+}
+	
+}
